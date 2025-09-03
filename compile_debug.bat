2f6463d0dd53d98e823436c8c9c5e1ade908103f
Alireza <zareian.a.r@gmail.com>
2025-09-02 20:55:17 -0400

    changed cmake to add zeromq lib.

--- compile_debug.bat
+++ compile_debug.bat
@@ -1,10 +1,13 @@
 @echo off
 REM ===========================================
-REM Compile.bat for Windows (Visual Studio + CMake) - Debug Mode
+REM Compile.bat for Windows (Visual Studio + CMake + vcpkg) - Debug Mode
 REM ===========================================
 
 REM Set build folder
 set BUILD_DIR=build
+
+REM Set path to vcpkg toolchain file
+set VCPKG_TOOLCHAIN=..\vcpkg\scripts\buildsystems\vcpkg.cmake
 
 REM Remove previous build folder if exists
 if exist %BUILD_DIR% (
@@ -16,9 +19,9 @@ REM Create build folder
 mkdir %BUILD_DIR%
 cd %BUILD_DIR%
 
-REM Configure CMake for Visual Studio 2022 (Debug)
+REM Configure CMake for Visual Studio 2022 (Debug) with vcpkg
 echo Configuring project with CMake...
-cmake -G "Visual Studio 17 2022" -A x64 ..
+cmake -G "Visual Studio 17 2022" -A x64 -DCMAKE_TOOLCHAIN_FILE=..\vcpkg\scripts\buildsystems\vcpkg.cmake ..
 
 REM Build Debug configuration
 echo Building Debug version...
